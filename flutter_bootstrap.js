6d469d4f66db0417742321597ba0a1230266a632
Russell Coke <russell.coke12@gmail.com>
2024-10-25 16:41:23 +0400

    added animation, added timestamp field when adding expense

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "194967578"
+    serviceWorkerVersion: "3171932780"
   }
 });
